39f9d926bc5506bd6c04c1a0300820846d98a4c5
AdelGann <imadelg14@gmail.com>
2025-07-16 22:41:10 -0400

    feat:(config): update autostart.sh
    add nitrogen, instead variety

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -10,12 +10,13 @@ PATH="$HOME/.config/qtile/scripts:$PATH"
 # dimension wide
 xrandr --output DP-1 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off --output DP-3 --off
 picom &
-feh --bg-fill $HOME/Imágenes/background/km_background.png
+#feh --bg-fill $HOME/Imágenes/background/km_background.png
 # start all this to entry the system
 sxhkd -c $HOME/.config/qtile/sxhkdrc &
 dunst -config "$HOME"/.config/qtile/dunstrc &
 # xss-lock -- i3lockmore -n --image-fill $HOME/Imágenes/background/surreal_art_by_thenumberoneai_dgfi3bl-fullview.jpg &
-run variety &
+#run variety &
+run nitrogen &
 run nm-applet &
 run blueman-applet &
 run pamac-tray &
